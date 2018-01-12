7edee7b11f68419f6d8c66d8606b3c05d5be52f2
sa <1182866004@qq.com>
2018-01-12 10:13:00 +0800

    去除多余代码

--- src/main/java/com/shiro/shirodemo/shiro/AuthRealm.java
+++ src/main/java/com/shiro/shirodemo/shiro/AuthRealm.java
@@ -39,50 +39,7 @@ public class AuthRealm extends AuthorizingRealm {
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals){
-        //获取当前登录的用户名,等价于(String)principals.fromRealm(this.getName()).iterator().next()
-        String currentUsername = (String)super.getAvailablePrincipal(principals);
-//      List<String> roleList = new ArrayList<String>();
-//      List<String> permissionList = new ArrayList<String>();
-//      //从数据库中获取当前登录用户的详细信息
-//      User user = userService.getByUsername(currentUsername);
-//      if(null != user){
-//          //实体类User中包含有用户角色的实体类信息
-//          if(null!=user.getRoles() && user.getRoles().size()>0){
-//              //获取当前登录用户的角色
-//              for(Role role : user.getRoles()){
-//                  roleList.add(role.getName());
-//                  //实体类Role中包含有角色权限的实体类信息
-//                  if(null!=role.getPermissions() && role.getPermissions().size()>0){
-//                      //获取权限
-//                      for(Permission pmss : role.getPermissions()){
-//                          if(!StringUtils.isEmpty(pmss.getPermission())){
-//                              permissionList.add(pmss.getPermission());
-//                          }
-//                      }
-//                  }
-//              }
-//          }
-//      }else{
-//          throw new AuthorizationException();
-//      }
-//      //为当前用户设置角色和权限
-//      SimpleAuthorizationInfo simpleAuthorInfo = new SimpleAuthorizationInfo();
-//      simpleAuthorInfo.addRoles(roleList);
-//      simpleAuthorInfo.addStringPermissions(permissionList);
-        SimpleAuthorizationInfo simpleAuthorInfo = new SimpleAuthorizationInfo();
-        //实际中可能会像上面注释的那样从数据库取得
-
-        System.out.println("_________________________"+currentUsername);
-        //添加一个角色,不是配置意义上的添加,而是证明该用户拥有admin角色
-        //添加权限
-        simpleAuthorInfo.addStringPermission("/admin/index.html");
-        simpleAuthorInfo.addStringPermission("/admin/left.html");
-        simpleAuthorInfo.addStringPermission("/admin/info.html");
-        simpleAuthorInfo.addStringPermission("/admin/user/list.html");
-        System.out.println("已为用户[mike]赋予了[admin]角色和[admin:manage]权限");
-        return simpleAuthorInfo;
-        //若该方法什么都不做直接返回null的话,就会导致任何用户访问/admin/listUser.jsp时都会自动跳转到unauthorizedUrl指定的地址
-        //详见applicationContext.xml中的<bean id="shiroFilter">的配置
+        return null;
     }
 
     /**
